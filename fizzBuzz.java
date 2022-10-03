7b9612793f77cf29d26bb2790c5dd8a335025616
Heyab Getachew <official.heyab@gmail.com>
2022-10-03 11:39:48 +0300

    fizzbuzz

---
+++ fizzBuzz.java
@@ -0,0 +1,16 @@
+class Solution {
+    public List<String> fizzBuzz(int n) {
+        LinkedList <String> output = new LinkedList<String>();
+        for(int i=1; i<=n; i++){
+            if(i%3==0 && i%5==0)
+                output.add("FizzBuzz");
+            else if(i%3==0)
+                output.add("Fizz");
+            else if(i%5==0)
+                output.add("Buzz");
+            else
+                output.add(i+"");
+        }
+        return output;
+    }
+}
