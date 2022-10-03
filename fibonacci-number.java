e68fca1ac34b0217931a5bec2f5b216721048d43
Heyab Getachew <official.heyab@gmail.com>
2022-10-03 15:05:05 +0300

    fibonacci-number

---
+++ fibonacci-number.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int fib(int n) {
+        if (n <= 0) return 0;
+      
+        int fibo[]=new int[n+1];
+        fibo[0] = 0; fibo[1] = 1;        
+      
+    
+        for (int i=2; i<=n; i++)
+            fibo[i] = fibo[i-1]+fibo[i-2];
+      
+      
+        return fibo[n];
+    }
+
+}
