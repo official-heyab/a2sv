37aaa85d34797a0bbea9adaf4ea2e90ed8e71714
Heyab Getachew <official.heyab@gmail.com>
2022-10-14 08:57:06 +0300

    mergeSort

--- selectionSort.java
+++ selectionSort.java
@@ -1,5 +1,5 @@
 
-public int[] selectionSort(int arr[]){
+public void selectionSort(int arr[]){
 	int n = arr.length;
     for (int i = 0; i < n-1; i++){
 		// Find the minimum element in unsorted array
@@ -13,7 +13,6 @@ public int[] selectionSort(int arr[]){
         arr[min_idx] = arr[i]; 
         arr[i] = temp;
 	}
-    return arr;
 }
 
 	
